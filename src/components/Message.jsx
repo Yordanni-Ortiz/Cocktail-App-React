e5d3283a817dbe3e275196efd7fd7316cc2f77ec
Yordanni Ortiz <yordannimod@gmail.com>
2023-01-13 20:06:09 -0400

    commit

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,10 @@
-
+import copa from "../data/img/fragil.png"
 
 const Message = () => {
     return (
-        <div>
-            <h2>Cualquier vaina</h2>
+        <div className="alert">
+            <img src={copa} alt="" className="copa"/>
+            <h2>Lo sentimos, no hay coincidencias en tu búsqueda.</h2>
         </div>
     )
 }
